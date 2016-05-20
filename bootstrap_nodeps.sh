2ca5bdb2aacbf1db020554e8f8b3a70509591445
José Sabater Montes <jsm@iaa.es>
2016-05-20 16:00:59 +0100

    Version without dependencies

---
+++ bootstrap_nodeps.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install af2_configure
+cd /home/vagrant
+wget http://www.ing.iac.es/astronomy/instruments/af2/af2_configure/af2_configure.tar.gz
+tar xfz af2_configure.tar.gz
+chown -R vagrant af2_configure
+
+# Install the dependencies of af2_configure
+sudo apt-get install -y pgplot5 tcsh
+wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/libg2c0_3.4.6-6ubuntu5_i386.deb
+sudo dpkg -i --force-all libg2c0_3.4.6-6ubuntu5_i386.deb
+
+# Update the configuration
+touch .profile
+echo "export PGPLOT_FONT=/usr/lib/pgplot5/grfont.dat" >> .profile
+echo "export PATH=$PATH:/home/vagrant/af2_configure/bin" >> .profile
